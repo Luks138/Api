d066c9f9ee849bf7b54edca9e70d7e4554dd6fa5
Golomidov.Y <golomidowyar@gmail.com>
2020-04-28 21:18:44 +0700

    Reconnected servlet-api library & fixes

--- src/yardani/servlets/ToApi.java
+++ src/yardani/servlets/ToApi.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class ToApi extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.sendRedirect("/api");
+        resp.sendRedirect("/index.jsp");
         return;
     }
 
